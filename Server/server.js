d1c394fb60e56064b85bdbd11de3d40c7a4c2322
Nasir-Mubarak007 <nmubarak717@gmail.com>
2024-06-01 16:04:29 +0100

    server updated

--- Server/server.js
+++ Server/server.js
@@ -22,10 +22,13 @@ app.use("/api/message", messageRoutes);
 
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/Client/build")));
+  app.use(express.static(path.join(__dirname1, "/Client/dist")));
+  pp.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
+  );
 } else {
   app.get("/", (req, res) => {
-    res.send("API is running");
+    res.send("API is running...");
   });
 }
 // -----------Deployment------------
