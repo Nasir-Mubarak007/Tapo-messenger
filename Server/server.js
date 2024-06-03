aec4c1282fe5366da8f01277fac96775fe6b6b85
Nasir Mubarak <125467460+Nasir-Mubarak007@users.noreply.github.com>
2024-06-03 17:23:33 +0100

    Update server.js
    removed merging server & client block

--- Server/server.js
+++ Server/server.js
@@ -18,20 +18,20 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-// -----------Deployment------------
+// // -----------Deployment------------
 
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/Client/dist")));
-  pp.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running...");
-  });
-}
-// -----------Deployment------------
+// const __dirname1 = path.resolve();
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/Client/dist")));
+//   pp.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running...");
+//   });
+// }
+// // -----------Deployment------------
 
 app.use(notFound);
 app.use(errorHandler);
