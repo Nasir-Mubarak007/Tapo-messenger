a862d09510f803e47021e0bb0b3b38f72e3c2c5f
Nasir Mubarak <125467460+Nasir-Mubarak007@users.noreply.github.com>
2024-06-04 15:07:01 +0100

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -39,18 +39,7 @@ if (process.env.NODE_ENV === "production") {
     res.send("API is running...");
   });
 }
-// -----------Deployment------------
-// const __dirname1 = path.resolve();
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "/Client/dist")));
-//   pp.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running...");
-//   });
-// }
+
 // // -----------Deployment------------
 
 app.use(notFound);
