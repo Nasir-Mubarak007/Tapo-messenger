003602ba335ada1d5d930af00615f7bd5e6e7da6
Nasir-Mubarak007 <nmubarak717@gmail.com>
2024-06-05 12:13:56 +0100

    renamed client to frontend and a branch render

--- Server/server.js
+++ Server/server.js
@@ -18,39 +18,20 @@ app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
-// app.get("/", (req, res) => {
-//   res.send("API is running...");
-// });
-
-// app.get("/", (req, res) => {
-//   res.send("API is running...");
-// });
-
 // // -----------Deployment------------
 
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/Client/dist")));
+  app.use(express.static(path.join(__dirname1, "/frontend/dist")));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "Client", "dist", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
     res.send("API is running...");
   });
 }
-// -----------Deployment------------
-// const __dirname1 = path.resolve();
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "/Client/dist")));
-//   pp.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running...");
-//   });
-// }
+
 // // -----------Deployment------------
 
 app.use(notFound);
